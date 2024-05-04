c8909f34a341b0317b90702ab97330cc5e122394
Oetgin <60893816+Oetgin@users.noreply.github.com>
2024-05-04 13:00:41 +0200

    Footer

--- src/static/footer.php
+++ src/static/footer.php
@@ -1,5 +1,20 @@
 <?php
 
 function includeFooter() {
-    echo '';
+    echo '
+    <footer>
+        <div class="box-left">
+            <p>Copyright ©2024 <b>GameView</b>. Tous droits réservés.</p>
+        </div>
+        <div class="box">
+            <p>France</p>
+        </div>
+        <div class="box-right">
+            <div class=authors>
+                <p>Théo Ginguené</p>
+                <p>Balthazar Girot</p>
+            </div>
+        </div>
+    </footer>
+    ';
 }
