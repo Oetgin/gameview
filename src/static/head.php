53125a42a5540062130fe03c1565c5534cf2e111
Oetgin <60893816+Oetgin@users.noreply.github.com>
2024-05-04 12:16:20 +0200

    Font fix

--- src/static/head.php
+++ src/static/head.php
@@ -25,6 +25,10 @@ function includeHead($stylesheet_path) {
 
         <link rel="icon" type="image/x-icon" href="'.LOGO_BLUE_PATH.'">
         <title>'.WEBSITE_TITLE.'</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     </head>
     ';
 }
