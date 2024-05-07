24ae6b890ab6c37ddd135f4a4df8ecca3a9f461b
bgirot <balthazar.girot@gmail.com>
2024-05-07 16:30:30 +0200

    article section and plus minus section done

--- src/pages/article.php
+++ src/pages/article.php
@@ -39,8 +39,8 @@ require_once(DOCUMENT_ROOT . '/src/components/article-card.php');
             <section class="title-section">
                 <div class="title-content">
                     <h2 class="game">Cyberpunk 2077</h2>
-                    <p class="title">"A masterpiece of the genre"</p>
-                    <p class="reviewer">A review by John Doe</p>
+                    <p class="title">"Le jeu de la décennie"</p>
+                    <p class="reviewer">Review par John Doe</p>
                 </div>
                 <div class="background-video">
                     <video class="boomerang" id="background-video" autoplay loop muted plays-inline>
@@ -85,17 +85,75 @@ require_once(DOCUMENT_ROOT . '/src/components/article-card.php');
                 </div>
             </section>
 
-            <img class="divider triangle" src="/assets/images/utils/triangle-divider.svg" alt="divider">
+            <img class="divider triangle" src="/assets/images/utils/triangle-divider-to-white.svg" alt="divider">
 
             <section class="article-section">
+                <h3 class="article-catch">Cyberpunk 2077 : Le jeu de la décennie</h3>
+                <p class="intro">Le lancement de Cyberpunk 2077 en décembre 2020 fut pour le moins chaotique, mais CD Projekt RED n'a jamais abdiqué. Au contraire, les studios polonais se sont attelés à rectifier le tir une update après l'autre afin de s'assurer que leur Action-RPG futuriste tienne toutes ses promesses. Avec la mise à jour 2.0, V accède-t-elle au statut de légende ? Il est grand temps de retourner à Night City en quête de réponses.</p>
+                
+                <h4 class="part-title">Un univers toujours plus “Premios”</h4>
+                <p class="corpus">
+                    Le monde ouvert bâti par CD Projekt RED était l'une des forces de Cyberpunk 2077 avec ses quartiers aux ambiances variées débordant de vie. Toutefois, cet univers imaginé à l'origine par Mike Pondsmith en 1988 ne pouvait pleinement s'épanouir sans une intelligence artificielle digne de ce nom. Si le comportement des citoyens lambda demeurent dans les grandes lignes inchangés, il en va autrement pour les ennemis qui se dressent sur le chemin de V. L'update 2.0 de CP 2077 a mis à jour leur logiciel ce qui leur permet d'opposer une résistance un peu plus coriace. Les paumards et autres edgerunners font parfois preuve d'initiative, et mettent en avant leurs atouts pour prendre le dessus lors des affrontements. Ces derniers utilisent régulièrement les piratages rapides, tentent de vous déloger avec des grenades et misent sur leurs forces pour vous éliminer. Les comportements demeurent encore artificiels, voire sommaires, mais la différence est notable. Néanmoins, ce sont les forces de l'ordre de Night City qui bénéficient de la mise à jour la plus significative.
+                </p>
+
+                <div class="image-container">
+                    <img class="image" src="/assets/images/illustrations/cyberpunk-illustration.jpg" alt="Cyberpunk ingame">
+                    <p class="caption">
+                        Depuis la mise à jour 2.0, Night City est plus réaliste que jamais. Le jeu nous offre un apeçu fascinant et effrayant des villes du futur.
+                    </p>
+                </div>
+                
+                <h4 class="part-title">Une personnalisation plus “Nova” que jamais</h4>
+                <p class="corpus">
+                    La personnalisation de V avait séduit les joueurs, mais restait perfectible sur de nombreux points. Conscients de cela, les développeurs proposent avec la mise à jour 2.0 une refonte totale des mécaniques RPG de Cyberpunk 2077 afin de coller au plus près de l'univers et donner une plus grande liberté aux mercenaires. Cela passe en premier lieu par un nouveau système d'avantages qui n'a plus rien à voir avec l'ancien, même s'il repose toujours sur les attributs du protagoniste (Constitution, Réflexes, Capacité Technique, Sang Froid et Intelligence). La majorité des attributs ont été modifiés tandis que d'autres font une entrée remarquée. Cette nouvelle approche a pour principal objectif de traduire par le gameplay vos choix et donc d'impacter l'expérience de jeu dans sa globalité. Il en va de même pour les implants qui deviennent encore plus essentiels. Point important, l'armure est désormais liée à votre matériel cybernétique et non à vos vêtements. Il est également conseillé d'améliorer votre matos en recyclant les composants, ce qui permet d’éviter de repasser à la caisse. CD Projekt RED s’assure ainsi que vos augmentations influent sur l’évolution de votre personnage, et c’est d’autant plus vrai avec la limitation mise en place. En effet, V ne peut supporter qu’une quantité limitée d’implants ce qui vous force à faire des choix parfois drastiques. Fort heureusement, cette limitation augmente en parallèle du gain d’expérience et garantit à votre mercenaire une destinée hors du commun. L’immersion se veut ludique, mais aussi visuelle. Les visites chez le charcudoc bénéficient d’une interface repensée et de cinématiques post-opératoires, histoire de ressentir un peu plus les changements effectués sur votre corps même si cela reste au final un gimmick plaisant. Enfin, la mise à jour 2.0 vous force à réattribuer vos points d'avantages déjà gagnés avec votre version de V, à moins que vous ne souhaitiez recommencer l'aventure du début... ce qui peut avoir son charme.
+                </p>
+                
+                <h4 class="part-title">Conclusion</h4>
+                <p class="corpus">
+                    Cyberpunk 2077 revient de loin, de très loin même, et sa résurrection est à la hauteur de ses débuts chaotiques. Avec les changements opérés par CD Projekt RED via la mise à jour 2.0, cet Action-RPG gomme les derniers errements des versions vanilla et 1.6, optimise ce qui devait l'être, et rend la personnalisation plus “Nova” que jamais. Assister à l'ascension de V dans les rues malfamées de Night City n'a jamais été aussi grisant. Cyberpunk 2077 renaît définitivement de ses cendres... et c'est un sans faute (ou presque) !
+                </p>
+
+                <div class="plus-minus-wrapper">
+                    <div class="plus-minus-container">
+                        <div class="plus-container column">
+                            <div class="column-title">
+                                Points forts
+                            </div>
+                            <ul>
+                                <li class="plus">L'expérience Cyberpunk 2077 sublimée</li>
+                                <li class="plus">Les interventions de la Police</li>
+                                <li class="plus">La refonte du système d'avantages et des implants</li>
+                            </ul>
+                        </div>
+
+                        <div class="plus-minus-divider"></div>
+
+                        <div class="minus-container column">
+                            <div class="column-title">
+                                    Points faibles
+                            </div>
+                            <ul>
+                                <li class="minus">L'IA ennemie toujours perfectible</li>
+                                <li class="minus">Le manque de nouveau contenu</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <img class="divider triangle" src="/assets/images/utils/triangle-divider-to-black.svg" alt="divider">
+
+            <section class="avis-section">
 
             </section>
+
         </main>
-    <script src="front.js"></script>
+
+        <?php includeFooter(); ?>
+
     </body>
 
     <?php
-        includeFooter();
         closeDB($mysqli);
     ?>
 
