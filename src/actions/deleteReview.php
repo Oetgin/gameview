3716aab309d675f5dd532e574fdcd99b9ad5f649
Oetgin <60893816+Oetgin@users.noreply.github.com>
2024-05-20 16:26:23 +0200

    Fixed redirects

--- src/actions/deleteReview.php
+++ src/actions/deleteReview.php
@@ -6,6 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+require_once(DOCUMENT_ROOT . '/src/utils/redirect.php');
 
 // Check for POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -15,7 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     require_once($_SERVER['DOCUMENT_ROOT'] . '/src/config/constants.php');
 
-    require_once(DOCUMENT_ROOT . '/src/utils/redirect.php');
 
     require_once(DOCUMENT_ROOT . '/src/config/dbConfig.php');
     require_once(DOCUMENT_ROOT . '/src/utils/dbQueries.php');
